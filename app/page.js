940d0775726e4943e27ef101e3dd12facbef5715
Gabriel Luna <glunamaia@gmail.com>
2026-08-28 12:48:07 -0300

    fix: correção na estrutura do projeto

--- app/page.js
+++ app/page.js
@@ -10,31 +10,28 @@ import Time from "./components/Time/Time";
 import Top from "./components/Top/top";
 import styles from './globals.css'
 
-export default function RootLayout({ children }) {
+export default function LandingPage() {
 
   const team = [
-    {nome: "Murilo Vieira dos Reis", rm: "RM573764", path: "/images/group/muriloV.jpeg"},
-    {nome: "Bruno Guilherme Gonçalves", rm: "RM573697", path: "/images/group/brunoG.jpeg"},
-    {nome: "Gabriel Luna Maia", rm: "RM570982", path: "/images/group/gabrielL.jpeg"},
-    {nome: "João Lucas Magordo", rm: "RM572419", path: "/images/group/joaoL.jpeg"},
-    {nome: "Gabriel Cardoso", rm: "RM571846", path: "/images/group/gabrielC.jpeg"},
+    { nome: "Murilo Vieira dos Reis", rm: "RM573764", path: "/images/group/muriloV.jpeg" },
+    { nome: "Bruno Guilherme Gonçalves", rm: "RM573697", path: "/images/group/brunoG.jpeg" },
+    { nome: "Gabriel Luna Maia", rm: "RM570982", path: "/images/group/gabrielL.jpeg" },
+    { nome: "João Lucas Magordo", rm: "RM572419", path: "/images/group/joaoL.jpeg" },
+    { nome: "Gabriel Cardoso", rm: "RM571846", path: "/images/group/gabrielC.jpeg" },
   ]
 
   return (
-    <html lang="en" style={styles}>
-      <body>
-        <Header />
-        <Top />
-        <Problem />
-        <Solution />
-        <Publico />
-        <Galeria />
-        <Caracteristicas />
-        <Time time={team} />
-        <Contato />
-        <Footer />
-        
-      </body>
-    </html>
+    <>
+      <Header />
+      <Top />
+      <Problem />
+      <Solution />
+      <Publico />
+      <Galeria />
+      <Caracteristicas />
+      <Time time={team} />
+      <Contato />
+      <Footer />
+    </>
   );
 }
